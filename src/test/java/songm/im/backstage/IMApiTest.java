068b9c0de9bde7dc63e4d95bfde60ae16f99e0a2
songzigw <songzigw@163.com>
2017-06-02 09:54:11 +0800

    backstage

--- src/test/java/songm/im/backstage/IMApiTest.java
+++ src/test/java/songm/im/backstage/IMApiTest.java
@@ -2,7 +2,6 @@ package songm.im.backstage;
 
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,51 +18,34 @@ public class IMApiTest {
     private static String uri = "http://127.0.0.1:17183/backstage";
     private static String key = "zhangsong";
     private static String secret = "1234567";
-    
+
     private IMApi api;
-    
+
     public IMApiTest() {
         IMApi.init(key, secret, uri);
         api = IMApi.getInstance();
     }
-    
+
     @BeforeClass
     public static void beforeClass() {
-        
+
     }
 
     @AfterClass
     public static void afterClass() {
-        
+
     }
-    
+
     @Before
     public void setUp() throws Exception {
-        
+
     }
-    
+
     @After
     public void tearDown() throws Exception {
-        
+
     }
-    
-    @Test
-    public void testGetToken() {
-        String uid = "10000";
-        String nick = "song";
-        String avatar = "";
-        Token t = null;
-        try {
-            t = api.getToken(uid, nick, avatar);
-        } catch (ApiException e) {
-            e.printStackTrace();
-        }
-        if (t != null) {
-            System.out.println(t.toString());
-        }
-        Assert.assertNotNull(t);
-    }
-    
+
     @Test
     public void testGetTokenUid10001() {
         String uid = "10001";
@@ -79,7 +61,7 @@ public class IMApiTest {
             System.out.println(t.toString());
         }
     }
-    
+
     @Test
     public void testGetTokenUid10002() {
         String uid = "10002";
